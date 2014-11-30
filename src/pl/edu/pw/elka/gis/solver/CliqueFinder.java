da9ed315d16cb610644ce752b2a7529e4d4766c4
Jacek Witkowski <jacek.witkowski@gmail.com>
2014-11-30 20:42:35 +0100

    Changed ArrayLists to LinkedLists (because of lower costs of removal). Changed variable names.

--- src/pl/edu/pw/elka/gis/solver/CliqueFinder.java
+++ src/pl/edu/pw/elka/gis/solver/CliqueFinder.java
@@ -8,17 +8,17 @@ import pl.edu.pw.elka.gis.domain.Clique;
 import pl.edu.pw.elka.gis.domain.Graph;
 import pl.edu.pw.elka.gis.domain.Node;
 
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 public class CliqueFinder {
     public List<Clique> findCliques(final Graph graph) {
-        final List<Clique> cliques = new ArrayList<>();
-        final List<Node> potentialClique = new ArrayList<>();
-        final List<Node> candidates = new ArrayList<>();
-        final List<Node> alreadyFound = new ArrayList<>();
+        final List<Clique> cliques = new LinkedList<>();
+        final List<Node> potentialClique = new LinkedList<>();
+        final List<Node> candidates = new LinkedList<>();
+        final List<Node> alreadyFound = new LinkedList<>();
         candidates.addAll(graph.getNodesList());
 
         findCliques(candidates, potentialClique, alreadyFound, cliques);
@@ -28,8 +28,8 @@ public class CliqueFinder {
     //TODO właściwie, to jest gotowe. Trzeba potestować.
     //TODO zaimplementować metodę bardziej skomplikowaną (z pivotem)
     private void findCliques(final List<Node> candidates, final List<Node> partialClique,
-                             final List<Node> alreadyFound, final List<Clique> cliques) {
-        if (candidates.isEmpty() && alreadyFound.isEmpty()) {
+                             final List<Node> skipped, final List<Clique> cliques) {
+        if (candidates.isEmpty() && skipped.isEmpty()) {
             cliques.add(new Clique(partialClique));
             return;
         }
@@ -38,14 +38,14 @@ public class CliqueFinder {
         while (iterator.hasNext()) {
             final Node candidate = iterator.next();
 
-            final List<Node> newPartialClique = new ArrayList<>(partialClique.size() + 1);
+            final List<Node> newPartialClique = new LinkedList<>();
             newPartialClique.addAll(partialClique);
             newPartialClique.add(candidate);
 
             final Set<Node> neighbours = candidate.getNeighbours();
             final List<Node> newCandidates = getIntersection(candidates, neighbours);
-            final List<Node> newAlreadyFound = getIntersection(alreadyFound, neighbours);
-            findCliques(newCandidates, newPartialClique, newAlreadyFound, cliques);
+            final List<Node> newSkipped = getIntersection(skipped, neighbours);
+            findCliques(newCandidates, newPartialClique, newSkipped, cliques);
 
             iterator.remove();
             partialClique.add(candidate);
@@ -53,7 +53,7 @@ public class CliqueFinder {
     }
 
     private List<Node> getIntersection(final List<Node> nodesList, final Set<Node> nodeSet) {
-        final List<Node> intersection = new ArrayList<>();
+        final List<Node> intersection = new LinkedList<>();
 
         for (final Node node : nodesList) {
             if (nodeSet.contains(node)) {

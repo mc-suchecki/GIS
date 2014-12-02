84c2100a0367606c604b91088c107ed89ba65439
Jacek Witkowski <jacek.witkowski@gmail.com>
2014-12-02 22:57:56 +0100

    Used maven. Changed directories structure.

--- src/pl/edu/pw/elka/gis/io/CliquesPrinter.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pw.elka.gis.io;
-
-import pl.edu.pw.elka.gis.domain.Clique;
-
-import java.util.Collection;
-
-public interface CliquesPrinter {
-    void print(Collection<Clique> clique);
-}
---
+++ src/test/java/pl/edu/pw/elka/gis/solver/degeneracysorter/DegeneracySorterTest.java
@@ -0,0 +1,16 @@
+package pl.edu.pw.elka.gis.solver.degeneracysorter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DegeneracySorterTest {
+    @Before
+    public void setUp() throws Exception {
+        //TODO create graph
+    }
+
+    @Test
+    public void testGetNodesSortedByDegeneracy() throws Exception {
+        //TODO create test
+    }
+}

7f78af7657b656430be53063f1565cd822939171
Jacek Witkowski <jacek.witkowski@gmail.com>
2015-01-08 20:57:57 +0100

    Fixed formating

--- src/main/java/pl/edu/pw/elka/gis/io/InvalidDataFormatException.java
+++ src/main/java/pl/edu/pw/elka/gis/io/InvalidDataFormatException.java
@@ -1,8 +1,9 @@
-package pl.edu.pw.elka.gis.io; /**
+package pl.edu.pw.elka.gis.io;
+
+/**
  * @author <a href="mailto:jacek.witkowski@gmail.com">Jacek Witkowski</a>
  * Created on 2014-11-2014.
  */
-
 public class InvalidDataFormatException extends Exception {
     public InvalidDataFormatException(final String message) {
         super(message);
